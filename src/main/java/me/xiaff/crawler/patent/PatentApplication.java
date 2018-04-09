8821dca744d0654e82635a47a2d7c9e755157554
夏方方 <loochenwei@gmail.com>
2018-04-09 04:54:52 -0700

    first commit

---
+++ src/main/java/me/xiaff/crawler/patent/PatentApplication.java
@@ -0,0 +1,12 @@
+package me.xiaff.crawler.patent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatentApplication.class, args);
+	}
+}
